5a2a20158813a419a5f55829400af27c513c6d5f
gustavotschoeke97 <gustavotschoeke@gmail.com>
2019-08-21 18:55:01 -0300

    first stable version

---
+++ calculos.js
@@ -0,0 +1,26 @@
+function tensaoMedia(){
+    let vp = document.getElementById("vp").value
+    let ang = document.getElementById("ang").value
+    try {
+        let Vm =vp*(1+Math.cos((ang*Math.PI)/180))/(2*Math.PI)
+        document.getElementById("vp")
+        document.getElementById("ang").innerHTML=''
+        return document.getElementById("response").innerHTML= Vm
+    } catch (error) {
+        return document.getElementById("response").innerHTML=error
+    }
+}
+
+function tensaoEficaz(){
+    let vp = document.getElementById("vp").value
+    let ang = document.getElementById("ang").value
+    console.log(vp,ang)
+    try {
+        let Vrms = vp*Math.sqrt(1/4 - ang/720 + Math.sin(((2*ang)*Math.PI/180))/(8*Math.PI))
+        return  document.getElementById('response').innerHTML= Vrms 
+    } catch (error) {
+        return document.getElementById('response').innerHTML=error
+    }
+}
+
+
